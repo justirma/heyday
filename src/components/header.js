decb582c6eedd283f4471d1b5068699f9bb47a3e
Caryssa Perez <caryssaperez@gmail.com>
2019-05-12 14:30:49 -0400

    Start header

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,50 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const navStyles = {
+  display: `inline-block`,
+  textAlign: `left`,
+  position: `absolute`,
+  width: `300px`,
+  marginTop: `10px`,
+  right: 0
+};
+
+const ulStyles = {
+  listStyleType: `none`,
+  position: `absolute`,
+  width: `auto`
+};
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, display: `inline-block` }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            width: `200px`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+      <nav style={navStyles}>
+        <ul style={ulStyles}>
+          <li style={{ display: `inline-block`, marginLeft: `6px`}}>How it works</li>
+          <li style={{ display: `inline-block`,  marginLeft: `16px`}}>Contact</li>
+        </ul>
+      </nav>
     </div>
   </header>
 )
